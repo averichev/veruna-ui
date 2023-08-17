0f9ee503b3e96e750d8f585c3fd17dca04635c84
averichev <averichev@yandex.ru>
2023-08-17 10:46:42 +0300

    improvement: Create user

--- src/view/UsersCreateView.vue
+++ src/view/UsersCreateView.vue
@@ -1,7 +1,40 @@
 <template>
   <div>
     <h1>Добавление пользователя</h1>
+    <form @submit.prevent="handleSubmit">
+      <div class="row">
+        <div class="col-12">
+          <label class="form-label" for="username">Имя пользователя</label>
+          <input
+            id="username"
+            v-model="formData.username"
+            type="text"
+            class="form-control"
+          />
+        </div>
+      </div>
+      <div>
+        <button class="btn" type="submit">Сохранить</button>
+      </div>
+    </form>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+import Default from "../plugins/httpClient.ts";
+import { useRouter } from "vue-router";
+
+const httpClient = new Default();
+const router = useRouter();
+
+const formData = reactive({
+  username: "",
+});
+const handleSubmit = async () => {
+  const payload = { ...formData };
+  await httpClient.post("/api/protected/user/create/", payload).then(() => {
+    router.push({ name: "users" });
+  });
+};
+</script>
